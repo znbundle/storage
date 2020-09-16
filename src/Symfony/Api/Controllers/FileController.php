d9226f6e2699f5946cdfc9024bf636be7d761378
Vitaliy <theyamshikov@yandex.ru>
2020-09-17 01:09:58 +0600

    upd

--- src/Symfony/Api/Controllers/FileController.php
+++ src/Symfony/Api/Controllers/FileController.php
@@ -4,7 +4,7 @@ namespace ZnBundle\Storage\Symfony\Api\Controllers;
 
 use ZnLib\Rest\Base\BaseCrudApiController;
 use ZnBundle\Storage\Domain\Interfaces\Services\FileServiceInterface;
-use ZnSandbox\Sandbox\Web\Symfony4\Traits\AccessTrait;
+use ZnLib\Web\Symfony4\WebBundle\Traits\AccessTrait;
 
 class FileController extends BaseCrudApiController
 {
