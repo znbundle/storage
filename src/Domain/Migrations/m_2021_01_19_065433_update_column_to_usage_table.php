801583abe44a2449269c0636bf7adf5a226152dc
Vitaliy <theyamshikov@yandex.ru>
2021-11-10 12:07:13 +0600

    upd

---
+++ src/Domain/Migrations/m_2021_01_19_065433_update_column_to_usage_table.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use ZnLib\Migration\Domain\Base\BaseColumnMigration;
+
+class m_2021_01_19_065433_update_column_to_usage_table extends BaseColumnMigration
+{
+
+    protected $tableName = 'storage_file_usage';
+
+    public function tableSchema()
+    {
+        return function (Blueprint $table) {
+            $table->integer('entity_id')->nullable()->change();
+        };
+    }
+}
