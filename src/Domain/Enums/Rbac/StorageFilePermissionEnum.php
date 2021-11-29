7fe8879b7b505f70727ac6e532c2ce94e031374a
Vitaliy <theyamshikov@yandex.ru>
2021-11-29 15:32:34 +0600

    upd

---
+++ src/Domain/Enums/Rbac/StorageFilePermissionEnum.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ZnBundle\Storage\Domain\Enums\Rbac;
+
+use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Contract\Rbac\Interfaces\GetRbacInheritanceInterface;
+use ZnCore\Contract\Rbac\Traits\CrudRbacInheritanceTrait;
+use ZnUser\Rbac\Domain\Enums\Rbac\SystemRoleEnum;
+
+class StorageFilePermissionEnum implements GetLabelsInterface, GetRbacInheritanceInterface
+{
+
+    use CrudRbacInheritanceTrait;
+
+    const CRUD = 'oStorageFileCrud';
+    const ALL = 'oStorageFileAll';
+    const ONE = 'oStorageFileOne';
+    const CREATE = 'oStorageFileCreate';
+    const UPDATE = 'oStorageFileUpdate';
+    const DELETE = 'oStorageFileDelete';
+    const RESTORE = 'oStorageFileRestore';
+
+    public static function getLabels()
+    {
+        return [
+            self::CRUD => 'Файлы. Полный доступ',
+            self::ALL => 'Файлы. Просмотр списка',
+            self::ONE => 'Файлы. Просмотр записи',
+            self::CREATE => 'Файлы. Загрузка файла',
+            self::UPDATE => 'Файлы. Редактирование',
+            self::DELETE => 'Файлы. Удаление',
+            self::RESTORE => 'Файлы. Восстановление',
+        ];
+    }
+
+    public static function getInheritance()
+    {
+        return [
+            self::CRUD => [
+                self::ALL,
+                self::ONE,
+                self::CREATE,
+                self::UPDATE,
+                self::DELETE,
+//                self::RESTORE,
+            ],
+            SystemRoleEnum::GUEST => [
+                self::CREATE,
+            ]
+        ];
+    }
+}
---
+++ src/Domain/Enums/Rbac/StorageServicePermissionEnum.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ZnBundle\Storage\Domain\Enums\Rbac;
+
+use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Contract\Rbac\Interfaces\GetRbacInheritanceInterface;
+use ZnCore\Contract\Rbac\Traits\CrudRbacInheritanceTrait;
+
+class StorageServicePermissionEnum implements GetLabelsInterface, GetRbacInheritanceInterface
+{
+
+    use CrudRbacInheritanceTrait;
+
+    const CRUD = 'oStorageServiceCrud';
+    const ALL = 'oStorageServiceAll';
+    const ONE = 'oStorageServiceOne';
+    const CREATE = 'oStorageServiceCreate';
+    const UPDATE = 'oStorageServiceUpdate';
+    const DELETE = 'oStorageServiceDelete';
+    const RESTORE = 'oStorageServiceRestore';
+
+    public static function getLabels()
+    {
+        return [
+            self::CRUD => 'Сервис файлохранилища. Полный доступ',
+            self::ALL => 'Сервис файлохранилища. Просмотр списка',
+            self::ONE => 'Сервис файлохранилища. Просмотр записи',
+            self::CREATE => 'Сервис файлохранилища. Создание',
+            self::UPDATE => 'Сервис файлохранилища. Редактирование',
+            self::DELETE => 'Сервис файлохранилища. Удаление',
+            self::RESTORE => 'Сервис файлохранилища. Восстановление',
+        ];
+    }
+}
