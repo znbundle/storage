a0230d92bbf0816834ffedf951dc1a5f3b3a885b
Vitaliy <theyamshikov@yandex.ru>
2022-05-07 17:53:28 +0600

    upd

--- src/Domain/Migrations/m_2021_01_19_053131_create_file_usage_table.php
+++ src/Domain/Migrations/m_2021_01_19_053131_create_file_usage_table.php
@@ -27,33 +27,6 @@ class m_2021_01_19_053131_create_file_usage_table extends BaseCreateTableMigrati
             $this->addForeign($table, 'service_id', 'storage_service');
             $this->addForeign($table, 'user_id', 'user_identity');
             $this->addForeign($table, 'file_id', 'storage_file');
-
-            /*if($this->isInOneDatabase('storage_service')) {
-                $table
-                    ->foreign('service_id')
-                    ->references('id')
-                    ->on($this->encodeTableName('storage_service'))
-                    ->onDelete(ForeignActionEnum::CASCADE)
-                    ->onUpdate(ForeignActionEnum::CASCADE);
-            }
-
-            if($this->isInOneDatabase('user_identity')) {
-                $table
-                    ->foreign('user_id')
-                    ->references('id')
-                    ->on($this->encodeTableName('user_identity'))
-                    ->onDelete(ForeignActionEnum::CASCADE)
-                    ->onUpdate(ForeignActionEnum::CASCADE);
-            }
-
-            if($this->isInOneDatabase('storage_file')) {
-                $table
-                    ->foreign('file_id')
-                    ->references('id')
-                    ->on($this->encodeTableName('storage_file'))
-                    ->onDelete(ForeignActionEnum::CASCADE)
-                    ->onUpdate(ForeignActionEnum::CASCADE);
-            }*/
         };
     }
 }
